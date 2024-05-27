58bb0fefab107515a9093e32d421bc0206a98225
Aurélien Collet <aurelien1722@gmail.com>
2024-05-27 17:18:34 +0200

    add main templates with layout and python file

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def homepage():
-    return "Hello, World!"
+    return render_template('homepage.html', title='Homepage')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
+
+@app.route('/account')
+def account():
+    return render_template('account.html', title='Account Profile')
+
+@app.route('/promotions')
+def promotions():
+    return render_template('promotions.html', title='Promotions')
 
 if __name__ == '__main__':
     app.run(debug=True)
