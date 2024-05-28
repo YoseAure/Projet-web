77b6ae11eca8c426436dfefd47151afb894fdf4e
Aurélien Collet <aurelien1722@gmail.com>
2024-05-28 16:50:18 +0200

    add css file for each page

--- main.py
+++ main.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def homepage():
-    return render_template('homepage.html', title='Homepage')
+    return render_template('homepage.html', title='Homepage', css_file='homepage.css')
 
 @app.route('/login')
 def login():
-    return render_template('login.html', title='Login')
+    return render_template('login.html', title='Login', css_file='login.css')
 
 @app.route('/register')
 def register():
-    return render_template('register.html', title='Register')
+    return render_template('register.html', title='Register', css_file='register.css')
 
 @app.route('/account')
 def account():
-    return render_template('account.html', title='Account Profile')
+    return render_template('account.html', title='Account Profile', css_file='account.css')
 
 @app.route('/promotions')
 def promotions():
-    return render_template('promotions.html', title='Promotions')
+    return render_template('promotions.html', title='Promotions', css_file='promotions.css')
 
 @app.route('/olympiades')
 def olympiade():
-    return render_template("olympiade.html", title="Olympiades")
+    return render_template("olympiade.html", title="Olympiades", css_file='olympiades.css')
 
 if __name__ == '__main__':
     app.run(debug=True)
