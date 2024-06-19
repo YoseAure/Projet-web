054a993a21c17cd9eb7f4445fa85f72557c66c31
Aurélien Collet <59016236+YoseAure@users.noreply.github.com>
2024-06-19 17:35:01 +0200

    Pofinage (#18)
    * fix account and edit account
    
    * fix register and login
    
    * add config file and fix register
    
    * update navbar
    
    * add return btn in edit profile and fix bug
    
    * add profile visualisation from other users from promotions
    
    * add id to go to the top of page

--- forms.py
+++ forms.py
@@ -1,34 +1,33 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-import email_validator
 
 
 class RegistrationForm(FlaskForm):
-    first_name = StringField('Prénom', validators=[
-                             DataRequired(), Length(min=2, max=30)])
-    last_name = StringField('Nom', validators=[
-                            DataRequired(), Length(min=2, max=30)])
+    first_name = StringField('Prénom', validators=[DataRequired(), Length(min=2, max=30)])
+    last_name = StringField('Nom', validators=[DataRequired(), Length(min=2, max=30)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Mot de passe', validators=[
-                             DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Confirmer mot de passe', validators=[
-                                     DataRequired(), EqualTo('Mot de passe')])
+    password = PasswordField('Mot de passe', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirmer mot de passe', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Créer mon compte')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Mot de passe', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('connexion')
+    remember = BooleanField('Se souvenir de moi')
+    submit = SubmitField('Se connecter')
+
 
 class EditProfileForm(FlaskForm):
-    phone = StringField('Téléphone', validators=[Length(min=0, max=20)])
-    address = StringField('Addresse', validators=[Length(min=0, max=255)])
-    company = StringField('Entreprise', validators=[Length(min=0, max=255)])
-    twitter = StringField('Twitter', validators=[Length(min=0, max=255)])
-    instagram = StringField('Instagram', validators=[Length(min=0, max=255)])
-    facebook = StringField('Facebook', validators=[Length(min=0, max=255)])
-    github = StringField('Github', validators=[Length(min=0, max=255)])
+    phone = StringField('Téléphone', validators=[Length(max=20)])
+    address = StringField('Adresse', validators=[Length(max=255)])
+    city = StringField('Ville', validators=[Length(max=255)])
+    postal_code = StringField('Code postal', validators=[Length(max=20)])
+    country = StringField('Pays', validators=[Length(max=255)])
+    company = StringField('Entreprise', validators=[Length(max=255)])
+    twitter = StringField('Twitter', validators=[Length(max=255)])
+    instagram = StringField('Instagram', validators=[Length(max=255)])
+    facebook = StringField('Facebook', validators=[Length(max=255)])
+    github = StringField('Github', validators=[Length(max=255)])
     submit = SubmitField('Sauvegarder')
--- main.py
+++ main.py
@@ -1,54 +1,42 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
-from flask_mysqldb import MySQL
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, current_user, logout_user, login_required
 from forms import RegistrationForm, LoginForm, EditProfileForm
-from werkzeug.security import check_password_hash
-import os
-from models import Promotion
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
-app.config['MYSQL_HOST'] = os.getenv('DB_HOST', 'enac.darties.fr')
-app.config['MYSQL_USER'] = os.getenv('DB_USER', 'aurelien.collet')
-app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD', 'u27dPvXHAzeUPzp4')
-app.config['MYSQL_DB'] = os.getenv('DB_NAME', 'les_apprentis')
-
-mysql = MySQL(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+from models import User, load_user, get_all_promotions
+from config import app, mysql, bcrypt, login_manager
 
 
-class User(UserMixin):
-    def __init__(self, id, first_name, last_name, email, password, promotion_id=None, phone=None, address=None, company=None, twitter=None, instagram=None, facebook=None, github=None, promotion_name=None, promotion_year=None):
-        self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.password = password
-        self.promotion_id = promotion_id
-        self.phone = phone
-        self.address = address
-        self.company = company
-        self.twitter = twitter
-        self.instagram = instagram
-        self.facebook = facebook
-        self.github = github
-        self.promotion_name = promotion_name
-        self.promotion_year = promotion_year
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('homepage'))
 
+    form = RegistrationForm()
+    promotions = get_all_promotions()
+    print(promotions)
 
-@login_manager.user_loader
-def load_user(user_id):
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT u.*, p.name, p.year FROM Users u LEFT JOIN Promotions p ON u.promotion_id = p.promotion_id WHERE u.user_id = %s", (user_id,))
-    user = cur.fetchone()
-    cur.close()
-    if user:
-        return User(user[0], user[1], user[2], user[3], user[4], user[5])
-    return None
+    if form.validate_on_submit():
+        first_name = form.first_name.data.strip().title()
+        last_name = form.last_name.data.strip().title()
+        email = form.email.data.strip()
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        promotion_id = int(request.form.get('annee'))
+        print(promotion_id)
+        print(type(promotion_id))
+
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO Users (first_name, last_name, email, password, promotion_id) VALUES (%s, %s, %s, %s, %s)",
+                        (first_name, last_name, email, hashed_password, promotion_id))
+            mysql.connection.commit()
+            cur.close()
+            flash('Votre compte a été créé avec succès! Vous pouvez maintenant vous connecter.', 'success')
+            return redirect(url_for('login'))
+        except Exception as e:
+            print('ici')
+            flash(f'Erreur lors de la création de votre compte: {e}', 'danger')
+            mysql.connection.rollback()
+
+    return render_template('register.html', title='Inscription', css_file='register.css', form=form, promotions=promotions)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -60,18 +48,18 @@ def login():
         email = form.email.data
         password = form.password.data
         cur = mysql.connection.cursor()
-        cur.execute("SELECT u.*, p.name, p.year FROM Users u LEFT JOIN Promotions p ON u.promotion_id = p.promotion_id WHERE u.email = %s", [email])
+        cur.execute("SELECT * FROM Users WHERE email = %s", [email])
         user = cur.fetchone()
         cur.close()
         if user and bcrypt.check_password_hash(user[4], password):
-            user_obj = User(id=user[0], first_name=user[1], last_name=user[2], email=user[3], password=user[4], promotion_id=user[5])
+            user_obj = User(id=user[0], first_name=user[1],
+                            last_name=user[2], email=user[3], password=user[4], promotion_id=user[5])
             login_user(user_obj)
-            flash('Logged in successfully.', 'success')
+            flash('Connexion réussie!', 'success')
             return redirect(url_for('account'))
         else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html', title='Login', css_file='login.css', form=form)
-
+            flash('Erreur de connexion. Veuillez vérifier votre email et mot de passe.', 'danger')
+    return render_template('login.html', title='Connexion', css_file='login.css', form=form)
 
 
 @app.route('/')
@@ -80,53 +68,41 @@ def homepage():
     return render_template('homepage.html', title='Homepage', css_file='homepage.css')
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('homepage'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(
-            form.password.data).decode('utf-8')
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO Users (first_name, last_name, email, password) VALUES (%s, %s, %s, %s)",
-                    (form.first_name.data, form.last_name.data, form.email.data, hashed_password))
-        mysql.connection.commit()
-        cur.close()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', css_file='register.css', form=form)
-
-
-@app.route('/account')
+@app.route('/account', defaults={'user_id': None})
+@app.route('/account/<int:user_id>')
 @login_required
-def account():
+def account(user_id):
+    # Si aucun user_id n'est fourni, afficher le profil de l'utilisateur actuel
+    if user_id is None:
+        user_id = current_user.id
+
+    # Obtenir les détails de l'utilisateur à afficher
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM Users WHERE user_id = %s", (current_user.id,))
+    cur.execute("SELECT * FROM Users WHERE user_id = %s", (user_id,))
     user = cur.fetchone()
-    prenom = user[1]
-    nom = user[2]
-    email = user[3]
+    if not user:
+        flash('User not found.', 'danger')
+        return redirect(url_for('promotions'))
+
+    prenom, nom, email = user[1], user[2], user[3]
     cur.close()
 
     cur = mysql.connection.cursor()
-    cur.execute(
-        "SELECT phone, address, company FROM UserDetails WHERE user_id = %s", (current_user.id,))
+    cur.execute("SELECT * FROM UserDetails WHERE user_id = %s", (user_id,))
     details = cur.fetchone()
-    phone = details[0] if details and details[0] else ''
-    address = details[1] if details and details[1] else ''
-    company = details[2] if details and details[2] else ''
+    phone = details[1] if details and details[1] else ''
+    address = details[2] if details and details[2] else ''
+    ville = details[3] if details and details[3] else ''
+    code_postal = details[4] if details and details[4] else None
+    pays = details[5] if details and details[5] else ''
+    company = details[6] if details and details[6] else ''
+    twitter = details[7] if details and details[7] else ''
+    instagram = details[8] if details and details[8] else ''
+    facebook = details[9] if details and details[9] else ''
+    github = details[10] if details and details[10] else ''
     cur.close()
 
-    cur = mysql.connection.cursor()
-    cur.execute(
-        "SELECT twitter, instagram, facebook, github FROM UserDetails WHERE user_id = %s", (current_user.id,))
-    social_links = cur.fetchone()
-    twitter = social_links[0] if social_links and social_links[0] else ''
-    instagram = social_links[1] if social_links and social_links[1] else ''
-    facebook = social_links[2] if social_links and social_links[2] else ''
-    github = social_links[3] if social_links and social_links[3] else ''
-    cur.close()
+    is_current_user = (user_id == current_user.id)
 
     return render_template('account.html',
                            title='Account Profile',
@@ -136,11 +112,15 @@ def account():
                            email=email,
                            phone=phone,
                            address=address,
+                           ville=ville,
+                           code_postal=code_postal,
+                           pays=pays,
                            company=company,
                            twitter=twitter,
                            instagram=instagram,
                            facebook=facebook,
-                           github=github)
+                           github=github,
+                           is_current_user=is_current_user)
 
 
 @app.route('/edit-profile', methods=['GET', 'POST'])
@@ -150,55 +130,122 @@ def edit_profile():
     if form.validate_on_submit():
         cur = mysql.connection.cursor()
 
-        cur.execute("""
-            INSERT INTO UserDetails (user_id, phone, address, company, twitter, instagram, facebook, github)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-            ON DUPLICATE KEY UPDATE
-                phone = VALUES(phone),
-                address = VALUES(address),
-                company = VALUES(company),
-                twitter = VALUES(twitter),
-                instagram = VALUES(instagram),
-                facebook = VALUES(facebook),
-                github = VALUES(github)
-        """, (current_user.id, form.phone.data, form.address.data, form.company.data, form.twitter.data, form.instagram.data, form.facebook.data, form.github.data))
+        phone = form.phone.data.strip() or None
+        address = form.address.data.strip() or None
+        city = form.city.data.strip() or None
+        postal_code = form.postal_code.data or None
+        country = form.country.data.strip() or None
+        company = form.company.data.strip() or None
+        twitter = form.twitter.data.strip() or None
+        instagram = form.instagram.data.strip() or None
+        facebook = form.facebook.data.strip() or None
+        github = form.github.data.strip() or None
 
-        mysql.connection.commit()
-        cur.close()
-        flash('Your profile has been updated!', 'success')
-        return redirect(url_for('account'))
+        try:
+            cur.execute("""
+                INSERT INTO UserDetails (user_id, phone, address, ville, code_postal, pays, company, twitter, instagram, facebook, github)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ON DUPLICATE KEY UPDATE
+                    phone = VALUES(phone),
+                    address = VALUES(address),
+                    ville = VALUES(ville),
+                    code_postal = VALUES(code_postal),
+                    pays = VALUES(pays),
+                    company = VALUES(company),
+                    twitter = VALUES(twitter),
+                    instagram = VALUES(instagram),
+                    facebook = VALUES(facebook),
+                    github = VALUES(github)
+            """, (current_user.id,
+                  phone,
+                  address,
+                  city,
+                  postal_code,
+                  country,
+                  company,
+                  twitter,
+                  instagram,
+                  facebook,
+                  github))
+            mysql.connection.commit()
+            flash('Your profile has been updated!', 'success')
+        except Exception as e:
+            flash(
+                f'Erreur lors de la mise à jour de votre profil : {e}', 'danger')
+            mysql.connection.rollback()
+        finally:
+            cur.close()
+
+        return redirect(url_for('account', user_id=current_user.id))
 
     # Pre-fill form with current data
     cur = mysql.connection.cursor()
-    cur.execute("SELECT phone, address, company, twitter, instagram, facebook, github FROM UserDetails WHERE user_id = %s", (current_user.id,))
+    cur.execute("SELECT * FROM UserDetails WHERE user_id = %s",
+                (current_user.id,))
     details = cur.fetchone()
     if details:
-        form.phone.data = details[0]
-        form.address.data = details[1]
-        form.company.data = details[2]
-        form.twitter.data = details[3]
-        form.instagram.data = details[4]
-        form.facebook.data = details[5]
-        form.github.data = details[6]
-
+        form.phone.data = details[1]
+        form.address.data = details[2]
+        form.city.data = details[3]
+        form.postal_code.data = details[4]
+        form.country.data = details[5]
+        form.company.data = details[6]
+        form.twitter.data = details[7]
+        form.instagram.data = details[8]
+        form.facebook.data = details[9]
+        form.github.data = details[10]
     cur.close()
 
     return render_template('edit-profile.html', title='Edit Profile', css_file='edit-profile.css', form=form)
 
 
-@app.route('/promotions')
+@app.route('/promotions', methods=['GET', 'POST'])
 @login_required
 def promotions():
-    promotions = Promotion.get_all_promotions(mysql)
     cur = mysql.connection.cursor()
-    cur.execute("SELECT p.name, p.year FROM Promotions p JOIN Users u ON u.promotion_id = p.promotion_id WHERE u.user_id = %s", (current_user.id,))
-    promotion = cur.fetchone()
+
+    # Récupération de toutes les promotions
+    cur.execute("""
+        SELECT p.promotion_id, p.year, p.name, COUNT(u.user_id) as user_count
+        FROM Promotions p
+        LEFT JOIN Users u ON p.promotion_id = u.promotion_id
+        GROUP BY p.promotion_id, p.year, p.name
+        ORDER BY p.year DESC""")
+    promotions = cur.fetchall()
+
+    cur.execute("SELECT p.name FROM Promotions p JOIN Users u ON u.promotion_id = p.promotion_id WHERE u.user_id = %s", (current_user.id,))
+    promotion_name = cur.fetchone()[0]
+    cur.execute("SELECT u.user_id, u.first_name, u.last_name FROM Users u WHERE u.promotion_id = %s", (current_user.promotion_id,))
+    users = []
+    users = cur.fetchall()
+
+
+    selected_promotion_id = request.form.get('promotion_id')
+    if selected_promotion_id:
+        cur.execute("SELECT u.user_id, u.first_name, u.last_name FROM Users u WHERE u.promotion_id = %s", (selected_promotion_id,))
+        users = cur.fetchall()
+        cur.execute("SELECT name FROM Promotions WHERE promotion_id = %s", (selected_promotion_id,))
+        promotion_name = cur.fetchone()[0]
+
     cur.close()
 
-    promotion_name = promotion[0] if promotion else "No promotion assigned"
-    promotion_year = promotion[1] if promotion else "N/A"
+    return render_template('promotions.html', title='Promotions', css_file='promotions.css',
+                           promotions=promotions, promotion_name=promotion_name, users=users)
 
-    return render_template('promotions.html', title='Promotions', css_file='promotions.css', promotions=promotions, promotion_name=promotion_name, promotion_year=promotion_year)
+
+# @app.route('/promotions')
+# @login_required
+# def promotions():
+#     promotions = Promotion.get_all_promotions(mysql)
+#     cur = mysql.connection.cursor()
+#     cur.execute("SELECT p.name, p.year FROM Promotions p JOIN Users u ON u.promotion_id = p.promotion_id WHERE u.user_id = %s", (current_user.id,))
+#     promotion = cur.fetchone()
+#     cur.close()
+
+#     promotion_name = promotion[0] if promotion else "No promotion assigned"
+#     promotion_year = promotion[1] if promotion else "N/A"
+
+#     return render_template('promotions.html', title='Promotions', css_file='promotions.css', promotions=promotions, promotion_name=promotion_name, promotion_year=promotion_year)
 
 
 @app.route('/olympiades')
@@ -229,29 +276,6 @@ def unfollow_user(user_id):
     return redirect(url_for('profile', user_id=user_id))
 
 
-@app.route('/profile/<int:user_id>')
-@login_required
-def profile(user_id):
-    cur = mysql.connection.cursor()
-
-    cur.execute("SELECT * FROM Users WHERE user_id = %s", (user_id,))
-    user = cur.fetchone()
-
-    cur.execute("SELECT Users.user_id, Users.prenom, Users.nom FROM UserFollowers "
-                "JOIN Users ON UserFollowers.followed_id = Users.user_id "
-                "WHERE UserFollowers.follower_id = %s", (user_id,))
-    following = cur.fetchall()
-
-    cur.execute("SELECT Users.user_id, Users.prenom, Users.nom FROM UserFollowers "
-                "JOIN Users ON UserFollowers.follower_id = Users.user_id "
-                "WHERE UserFollowers.followed_id = %s", (user_id,))
-    followers = cur.fetchall()
-
-    cur.close()
-
-    return render_template('profile.html', user=user, following=following, followers=followers)
-
-
 @app.route('/send_message', methods=['POST'])
 @login_required
 def send_message():
@@ -276,7 +300,7 @@ def old_messages():
     return render_template('old-messages.html', css_file="messages.css", title='old messages')
 
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 @login_required
 def messages():
     user_id = current_user.id
@@ -321,7 +345,6 @@ def messages():
         other_user=other_user,
         css_file='messages.css'
     )
-
 
 
 @app.route('/logout')
